ffe7fb2c9adcee7d7f21d6e863e554c97b524cf7
Programador Novato <info@programadornovato.com>
2019-07-20 00:23:14 -0500

    02.- Ajax con Jquery🌇  de 0 a 100 [funciónn get con json]
    Video:
    https://youtu.be/a_CAMcYqVzM

--- javascript.js
+++ javascript.js
@@ -3,8 +3,19 @@ $(document).ready(function () {
         e.preventDefault();
         $.get("archivo.txt",function(data,textStatus,jqXHR){
             console.log(data);
-            console.log(textStatus);
-            console.log(jqXHR);
+            //console.log(textStatus);
+            //console.log(jqXHR);
+        });
+    });
+    $('#leerEmpleado').click(function (e) { 
+        e.preventDefault();
+        $.get("empleados.json",function(data){
+            console.log(data);
+            $('#datosEmpleado').html(`
+            Nombre: ${data.nombre} <br>
+            Puesto: ${data.puesto} <br>
+            Edad: ${data.edad} <br>
+            `);
         });
     });
 });
