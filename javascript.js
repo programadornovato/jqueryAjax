cfbcb83464a2a71d5bf041f762adeefa48f075b5
Programador Novato <info@programadornovato.com>
2019-07-21 00:47:23 -0500

    07.- Ajax con Jquery🌇  de 0 a 100 [ funcion ajax ]
    Video:
    https://youtu.be/pIQsJ84kUWg

--- javascript.js
+++ javascript.js
@@ -1,68 +1,24 @@
 $(document).ready(function () {
-    $("#leer").click(function (e) { 
-        e.preventDefault();
-        $.get("archivo.txt",function(data,textStatus,jqXHR){
-            console.log(data);
-            //console.log(textStatus);
-            //console.log(jqXHR);
-        });
-    });
-    $('#leerEmpleado').click(function (e) { 
-        e.preventDefault();
-        $.get("empleados.json",function(data){
-            console.log(data);
-            $('#datosEmpleado').html(`
-            Nombre: ${data.nombre} <br>
-            Puesto: ${data.puesto} <br>
-            Edad: ${data.edad} <br>
-            `);
-        });
-    });
-    $('#leerArregloEmpleado').click(function (e) { 
+    $('#ajax').click(function (e) { 
         e.preventDefault();
         $('#listaEmpleados').html('');
-        $.get("empleados.json",function(data){
-            //console.log(data);
-            $.each(data, function (index, item) { 
-                 $('#listaEmpleados').html($('#listaEmpleados').html()+`
-                 <li> ${item.nombre} -- ${item.puesto} </li>
-                 `);
-            });
-        });
-    });
-    $('#leerGetSON').click(function (e) { 
-        e.preventDefault();
-        /*
-        $.getJSON("empleados.txt",function(data){
-            data=JSON.parse(data);
-            console.log(data);
-        });
-        */
-        $.getJSON("empleados.json",function(data){
-            console.log(data.empleados);
-            $('#listaEmpleados').html('');
-            $.each(data.temporales, function (index, item) { 
-                $('#listaEmpleados').html($('#listaEmpleados').html()+`
-                <li> ${item.nombre} -- ${item.puesto} </li>
-                `);
-           });
-        });
-    });
-
-    let empleados;
-    $.getJSON("empleados.json",function(data){
-        empleados=data.empleados;
-    });
-    $('#nombre').keyup(function (e) { 
-        $('#listaEmpleados').html('');
-        let nombre=$(this).val();
-        $.each(empleados, function (indexInArray, item) { 
-            if(item.nombre.toLowerCase().indexOf(nombre.toLowerCase())!==-1){
-                $('#listaEmpleados').html($('#listaEmpleados').html()+`
-                <li> ${item.nombre} -- ${item.puesto} </li>
-                `);
+        $.ajax({
+            url:'empleados.json',
+            type:'get',
+            dataType:'json',
+            success:function(data){
+                console.log(data);
+                $.each(data.empleados, function (i, item) { 
+                     $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                     <li> ${item.nombre} </li>
+                     `);
+                });
+            },
+            error:function(xhr,status,error){
+                console.log(xhr);
+                console.log(status);
+                console.log(error);
             }
         });
     });
-
 });
