3e8ceaaa50b9aae01d2276a6e29dd4851c23a519
Programador Novato <info@programadornovato.com>
2019-07-22 02:02:22 -0500

    10.- Ajax con Jquery🌇  de 0 a 100 [ done, fail y always ]
    Video:
    https://youtu.be/I5gKXjD24Jc

--- javascript.js
+++ javascript.js
@@ -6,8 +6,19 @@ $(document).ready(function () {
         $.post("http://localhost/ajax/convertir.php", 
         {'nombre':nombre,'puesto':puesto},
             function (data, textStatus, jqXHR) {
+                $('#res').parent().removeClass('d-none');
                 $('#res').html(data);
             }
-        );
+        ).done(function(){
+            $('#estado').html("Exito");
+            $('#estado').addClass('alert-success');
+            $('#estado').removeClass('d-none');
+        }).fail(function(){
+            $('#estado').html("Fallo");
+            $('#estado').addClass('alert-danger');
+            $('#estado').removeClass('d-none');
+        }).always(function(){
+            console.log("finalizo");
+        });
     });
 });
