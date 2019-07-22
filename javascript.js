36551abd009432b8f03e133ff3d5db45cd42790d
Programador Novato <info@programadornovato.com>
2019-07-22 07:56:49 -0500

    13.- Ajax con Jquery🌇  de 0 a 100 [CRUD Crear]
    Video:
    https://youtu.be/o_Tt35D6nnM

--- javascript.js
+++ javascript.js
@@ -3,6 +3,7 @@ $(document).ready(function () {
         e.preventDefault();
         $('#listaEmpleados').html('');
         $.getJSON('http://localhost/ajax/empleados.php',
+        {"accion":"leer"},
         function(data){
             //console.log(data);
             $.each(data, function (index, item) { 
@@ -12,4 +13,21 @@ $(document).ready(function () {
             });
         });
     });
+    $('#crearEmpleado').click(function (e) { 
+        e.preventDefault();
+        let nombre=$('#nombre').val();
+        let puesto=$('#puesto').val();
+        let edad=$('#edad').val();
+        $.post('http://localhost/ajax/empleados.php',
+        {"accion":"insertar","nombre":nombre,"puesto":puesto,"edad":edad},
+        function(data){
+            if(data==="1"){
+                $('#nombre').val('');
+                $('#puesto').val('');
+                $('#edad').val('');        
+            }
+        });
+
+    });
+
 });
