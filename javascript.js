7f2ba2437af0c0382383cd952ba4d92832d088c6
Programador Novato <info@programadornovato.com>
2019-07-22 05:22:09 -0500

    12.- Ajax con Jquery🌇  de 0 a 100 [php, mysql y ajax]
    Video:
    https://youtu.be/KbO7hwTe2as

--- javascript.js
+++ javascript.js
@@ -1,24 +1,15 @@
 $(document).ready(function () {
-    $('form').submit(function (e) { 
+    $('#leerEmpleados').click(function (e) { 
         e.preventDefault();
-        let nombre=$('#nombre').val();
-        let puesto=$('#puesto').val();
-        $.post("http://localhost/ajax/convertir.php", 
-        {'nombre':nombre,'puesto':puesto},
-            function (data, textStatus, jqXHR) {
-                $('#res').parent().removeClass('d-none');
-                $('#res').html(data);
-            }
-        ).done(function(){
-            $('#estado').html("Exito");
-            $('#estado').addClass('alert-success');
-            $('#estado').removeClass('d-none');
-        }).fail(function(){
-            $('#estado').html("Fallo");
-            $('#estado').addClass('alert-danger');
-            $('#estado').removeClass('d-none');
-        }).always(function(){
-            console.log("finalizo");
+        $('#listaEmpleados').html('');
+        $.getJSON('http://localhost/ajax/empleados.php',
+        function(data){
+            //console.log(data);
+            $.each(data, function (index, item) { 
+                 $('#listaEmpleados').html($('#listaEmpleados').html()+`
+                 <li> ${item.nombre} -- ${item.puesto} -- ${item.edad} </li>
+                 `);
+            });
         });
     });
 });
